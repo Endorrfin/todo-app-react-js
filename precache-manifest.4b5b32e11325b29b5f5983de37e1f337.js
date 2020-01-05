f8624f0845d60e98328507814c103c2cda4bc576
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-05 11:33:31 +0200

    Updates

---
+++ precache-manifest.4b5b32e11325b29b5f5983de37e1f337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f769a36a679041de9af4082acea7c41d",
+    "url": "/todo-app-react-js/index.html"
+  },
+  {
+    "revision": "a0d2debc559ecafc06be",
+    "url": "/todo-app-react-js/static/css/main.e1d48528.chunk.css"
+  },
+  {
+    "revision": "10075bfb33f02be50c10",
+    "url": "/todo-app-react-js/static/js/2.4afefce7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app-react-js/static/js/2.4afefce7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0d2debc559ecafc06be",
+    "url": "/todo-app-react-js/static/js/main.c4b92aee.chunk.js"
+  },
+  {
+    "revision": "2c06856fc419c25d571f",
+    "url": "/todo-app-react-js/static/js/runtime-main.f4ab91f6.js"
+  }
+]);
